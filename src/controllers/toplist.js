e271a6c8d58b30311e72eb800dec1a4b3704790d
EasierChan <chenlei@hongkingsystem.cn>
2016-07-22 14:17:50 +0800

    修改toplist

--- src/controllers/toplist.js
+++ src/controllers/toplist.js
@@ -6,16 +6,12 @@ const IPCMSG = require('../models/qtpmodel').IPCMSG;
 
 angular.module("app_toplist", ['ui.bootstrap', 'ngAnimate'])
     .controller('c_parent', ['$scope', function ($scope) {
-        // $scope.currentPage = 1;
-        // $scope.totalItems = 20;
-
-        // $scope.pageChange= function(){
-        // }
         $scope.shareObject = new Object();
         $scope.shareObject.header = [];
         $scope.shareObject.columns = [];
-    }])
-    .controller('c_config', ['$scope', function ($scope) {
+        $scope.shareObject.rankMin = 1;
+        $scope.shareObject.rankMax = 30;
+        
         $scope.oneAtATime = true;
         $scope.status = {
             isFirstOpen: true,
@@ -24,48 +20,68 @@ angular.module("app_toplist", ['ui.bootstrap', 'ngAnimate'])
             aopen: true
         };
 
-        ipcRenderer.send(IPCMSG.BackendPoint, { reqno: 1, msgtype: QtpConstant.MSG_TYPE_TOPLIST });
-        ipcRenderer.once(IPCMSG.FrontendPoint, function (e, arg) {
-            console.log(arg);
-            $scope.topArr = arg.toplisttype;
-        });
 
         var reqObj = {
             reqno: 1,
             msgtype: QtpConstant.MSG_TYPE_TOPLIST_BASE,
-            ranke: [2, 5],
+            ranke: [$scope.shareObject.rankMin, $scope.shareObject.rankMax],
             column: [],
             filter: []
         };
-
-
+        
+        ipcRenderer.send(IPCMSG.BackendPoint, { reqno: 1, msgtype: QtpConstant.MSG_TYPE_TOPLIST });
+        ipcRenderer.once(IPCMSG.FrontendPoint, function (e, arg) {
+            console.log(arg);
+            reqObj.column.push(arg.toplisttype[0].option[0].fieldname);
+            reqObj.column.push(arg.toplisttype[0].option[1].fieldname);
+            $scope.shareObject.header.push(arg.toplisttype[0].option[0].columnname);
+            $scope.shareObject.header.push(arg.toplisttype[0].option[1].columnname);
+            arg.toplisttype[0].option.splice(0,2);
+            $scope.topArr = arg.toplisttype;
+        });
+        
+        var minInterval = null;
         $scope.reqToplist = function () {
+            minInterval = intervals.sort(function(a,b){
+                return parseInt(a) - parseInt(b);
+            })[0];
+            console.log(minInterval);
             //console.log(reqObj.column);
             //console.log($scope.shareObject.header);
             $scope.shareObject.columns = reqObj.column;
+            reqObj.ranke = [parseInt($scope.shareObject.rankMin), parseInt($scope.shareObject.rankMax)];
+            console.log(reqObj);
             ipcRenderer.send(IPCMSG.BackendPoint, reqObj);
             angular.element(document.getElementById("toplist_config")).removeClass("current").addClass("future");
             angular.element(document.getElementById("toplist_content")).removeClass("future").addClass("current");
         };
 
         var idx;
-        $scope.setColumn = function (colID, text, bCheck) {
+        var intervals = new Array();
+        $scope.setColumn = function (colID, text, p_interval, bCheck) {
             if (bCheck != undefined) {
                 if (bCheck) {
                     if (reqObj.column.indexOf(colID) < 0) {
                         reqObj.column.push(colID);
                         $scope.shareObject.header.push(text);
+                        if(p_interval > 0 && intervals.indexOf(p_interval) < 0){
+                            intervals.push(p_interval);
+                        }
                     }
                 } else {
                     if ((idx = reqObj.column.indexOf(colID)) >= 0) {
                         reqObj.column.splice(idx, 1);
                         $scope.shareObject.header.splice(idx, 1);
+                        if((idx = intervals.indexOf(p_interval)) >= 0){
+                            intervals.splice(idx, 1);
+                        }
                     }
                 }
             }
         };
-    }])
-    .controller('c_topcontent', ['$scope', function ($scope) {
+        
+        /////
+        ////////content
         $scope.rows = [];
         ipcRenderer.on(IPCMSG.FrontendPoint, function (e, res) {
             if (res.msgtype != QtpConstant.MSG_TYPE_TOPLIST_BASE) {
@@ -76,11 +92,40 @@ angular.module("app_toplist", ['ui.bootstrap', 'ngAnimate'])
                 //console.log(obj);
                 $scope.rows[index] = new Array();
                 for (var col in $scope.shareObject.columns) {
-                    //console.log(col, $scope.shareObject.columns[col]);
                     $scope.rows[index].push(eval("obj." + $scope.shareObject.columns[col]));
                 }
+                
+                // for(var prop in obj){
+                //     if($scope.shareObject.columns.indexOf(prop) > -1){
+                //         $scope.rows[index].push(obj[prop]);
+                //     }
+                // }
             });
             console.log($scope.rows);
             $scope.$apply();
+            
+            // setTimeout(function(){
+            //     ipcRenderer.send(IPCMSG.BackendPoint, reqObj);
+            // }, minInterval*1000);
         })
+        
+        var headerMap = new Object();
+        
+        $scope.setOption = function(colHeader, colIndex){
+            var obj = new Object();
+            var headerID = reqObj.column[colIndex];
+            obj[headerID] = new Object();
+            if(!headerMap.hasOwnProperty(colHeader)){
+                headerMap[colHeader] = false;
+                obj[headerID]["asc"] = "";
+            } else {
+                headerMap[colHeader] = !(headerMap[colHeader]);
+                obj[headerID]["desc"] = "";
+            }
+            
+            reqObj.filter.push(obj);
+            console.log(reqObj);
+            ipcRenderer.send(IPCMSG.BackendPoint, reqObj);
+            obj = null;
+        }
     }]);
